f8d861b7de5192c38bdc6c10a153ccf4fa85e106
AhmadTtawfiq Muhammad <144332302+Tawfiqm857@users.noreply.github.com>
2025-08-19 01:52:50 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/jetechhub/", // 👈 important for GitHub Pages
   server: {
     host: "::",
-    port: 8080,
+    port: 8080, // 👈 set a port (default is 5173, you can change if needed)
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
